3a51ffed0549e81568c97b251de5fe5049a4cbba
Ubuntu <ubuntu@ip-172-31-19-197.us-west-2.compute.internal>
2017-08-24 17:09:10 +0000

    removed bottleneck files and nohup from training. Using flask for server.

--- myserver.py
+++ myserver.py
@@ -1,7 +1,24 @@
 import tensorflow as tf
-import socketserver as ss
+from flask import Flask, request, jsonify, abort
+app = Flask(__name__)
 
-def run_graph(image_data, labels, input_layer_name, output_layer_name, num_top_predictions):
+input_layer_name = "DecodeJpeg/contents:0"
+output_layer_name = "final_result:0"
+
+def load_graph(filename):
+	"""Unpersists graph from file as default graph."""
+	with tf.gfile.FastGFile(filename, 'rb') as f:
+		graph_def = tf.GraphDef()
+		graph_def.ParseFromString(f.read())
+		tf.import_graph_def(graph_def, name='')
+
+labels = [line.rstrip() for line in tf.gfile.GFile("my_labels.pb")]
+load_graph("my_graph.pb")
+
+def run_graph(image_data, num_top_predictions):
+	global input_layer_name
+	global output_layer_name
+	global labels
 	with tf.Session() as sess:
 		# Feed the image_data as input to the graph.
 		#   predictions  will contain a two-dimensional array, where one
@@ -12,26 +29,28 @@ def run_graph(image_data, labels, input_layer_name, output_layer_name, num_top_p
 
 		# Sort to show labels in order of confidence
 		top_k = predictions.argsort()[-num_top_predictions:][::-1]
+		results = []
 		for node_id in top_k:
 			human_string = labels[node_id]
 			score = predictions[node_id]
-			print('%s (score = %.5f)' % (human_string, score))
-		return 0
+			score_print = '%s (score = %.5f)' % (human_string, score)
+			results.append((human_string, score, score_print))
+		return results
 
 
-class MyServer(ss.BaseRequestHandler):
-	def handle(self):
-		self.data = self.request.recv(1024).strip()
-		print("{} wrote:".format(self.client_address[0]))
-		print(self.data)
-		# just send back the same data, but upper-cased
-		self.request.sendall(self.data.upper())
+@app.route("/")
+def hello():
+	return "Hello world!"
 
 
-if __name__ == "__main__":
-	HOST, PORT = "localhost", 9999
+@app.route("/get_prediction")
+def get_prediction():
+	if "image" not in request.files:
+		abort(400, "Couldn't find an image file.")
+	results = run_graph(request.files["image"], 5)
+	return jsonify(results)
 
-	# Create the server, binding to localhost on port 9999
-	server = ss.TCPServer((HOST, PORT), MyServer)
-	server.serve_forever()
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
+	
 
